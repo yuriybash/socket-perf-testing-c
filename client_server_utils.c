aca5e8938e5147440e2666684a7b269580c7efeb
Yuriy Bash <yuriybash@gmail.com>
2018-07-22 18:14:37 -0400

    comments for client_server_utils.c

--- client_server_utils.c
+++ client_server_utils.c
@@ -1,15 +1,21 @@
 /* File of shared functions between the server and the client
- * Author: YOUR NAME HERE */
+ * Author: Yuriy Bash */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "client_server_utils.h"
 
-
-/* Function that finds the index at which the newline character exists in the
- * message. Returns -1 if no newline exists in the string. START is in the
- * index in the start at which the search should begin. */
+ /*
+  *  Function: find_message_end
+  *  --------------------------
+  *  finds the index at which the newline character exists in the message
+  *
+  *  msg: pointer to start of message
+  *  start: index at which to start search
+  *
+  *  returns: the index of the newline delimiter. -1 if newline not found.
+  */
 int find_message_end (char *msg, int start){
 
         char* current_char;
@@ -23,22 +29,18 @@ int find_message_end (char *msg, int start){
         return -1;
 }
 
-// "hello this is message 1\nAND this is message two\nTHIS IS THREE\0"
-/* Function that takes a char * which is a buffer of data received. This 
- * buffer contains 1 or MESSAGES where a message is defined as being a 
- * series of characters ending in a newline. Its other parameter, end,
- * is the length of the first message in the buffer. If MESSAGES contains more 
- * than one message then these messages WILL NOT be separated by a null
- * terminator. However, MESSAGES will always be terminated by a null 
- * terminator. The function should malloc and outputs a new char * consisting 
- * of the first message (which needs end + 1 Bytes). It should also update 
- * MESSAGES so that its contents are a string that contains any remaining 
- * messages and just a null terminator if no more messages remain. You may 
- * want to use some combination of strlen, strcpy, and strncpy. As an 
- * important note if you elect to attempt to use strcpy or strncpy for this 
- * function the dst pointer and the src pointer cannot contain addresses that 
- * will overlap. This means the address of any information from src cannot be 
- * the same as an address that gets written to in dst */
+/*
+ * Function: generate_message
+ * --------------------------
+ * given a buffer that contains 1-n messages delimited by '\n', return the
+ * first message.
+ *
+ * messages: pointer to buffer
+ * end: index at which first message ends (including the '\n' delimiter)
+ *
+ * returns: the first message (including its last char, '\n')
+ *
+ */
 char *generate_message (char *messages, int end) {
 
     char* extracted_message = (char*) malloc((end+1)*sizeof(char));
@@ -56,7 +58,14 @@ char *generate_message (char *messages, int end) {
 
 }
 
-/* Function that terminates the program when a malloc fails. */
+/*
+ * Function: allocation_failed
+ * ---------------------------
+ *
+ * clean up when a malloc fails.
+ *
+ * returns: void
+ */
 void allocation_failed () {
         fprintf (stderr, "Unable to allocate enough memory\n");
         exit (1);
