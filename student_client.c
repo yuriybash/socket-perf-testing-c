b7748f8d76efbe5121e85f84b11c94c4bceea282
Yuriy Bash <yuriybash@gmail.com>
2018-07-23 00:34:30 -0400

    cleans up student_client.c

--- student_client.c
+++ student_client.c
@@ -1,5 +1,5 @@
 /* A file of functions involving processing messages for students to implement.
- * Author: YOUR NAME HERE */
+ * Author: Yuriy Bash */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,23 +17,19 @@ char input_message[MAX_INPUT_LENGTH + 1];
 /* Variable that holds the next available spot in input_message. */
 unsigned input_offset;
 
- /* Processes input from stdin to be written to the server using send_message.
- * If only part of a message has been written then the information should be
- * stored in the input_message array and the next available location stored at
- * input_offset. In particular we will assume that a complete message will
- * never exceed MAX_INPUT_LENGTH (a macro defined in client_server_utils.h
- * which includes the space for the '\n' and null termininator) and all 
- * complete messages will be terminated with a newline character. To do 
- * this you will likely want to use the built in getc function. One 
- * important note about the getc function is that since we are reading from 
- * stdin while it is non-blocking (meaning we do not wait for input to arrive)
- * if no input is available getc will return the MACRO EOF, which has the 
- * value -1. You will need to explicitly check for this as otherwise our 
- * autograder my note be able to have your client receive messages. Finally, 
- * don't forget to readd the "[Me:]" prefix after the message is sent. 
- * You may find a display_prefix helpful. To send a message you will want
- * to use the send_message function that is provided for you in client_utils.h.
- * Don't forget to null terminate any string you send. */
+
+/*
+ * Function: process_input
+ * -----------------------
+ *
+ * Processes input from stdin to be written to the server using send_message.
+ *
+ * If only part of a message has been written, the information is stored in
+ * input_message at the input_offset index. Messages are terminated with '\n'
+ * _and_ '\0'.
+ *
+ * returns: void
+ */
 void process_input () {
      int c;
      input_offset = 0;
@@ -53,15 +49,21 @@ void process_input () {
 
 }
 
-/* Function that takes in a whole message from a server and handles
- * it appropriately. A message from a server will have a leading byte that
- * indicates what type of message it is. This will either be a
- * Standard_Message or an Exit_Message (see the enum in client_server_utils.h).
- * If the message is an Exit_Message the user should call the 
- * handle_exit function to exit appropriately. Otherwise the message should 
- * be output (without the message type byte). Be sure and remove and readd 
- * the "[Me:]" prefix. You may find the clear_prefix and display_prefix helper
- * functions helpful. */
+
+/*
+ * Function: handle_server_message
+ * -----------------------
+ *
+ * Function that takes in a whole message from a server and handles it
+ * appropriately. A message from a server will have a leading byte that
+ * indicates what time of message it is (either Standard_Message or
+ * Exit_Message, see the enum in client_server_utils.h). If exit, handle_exit
+ * is called, otherwise the message is output (without leading byte).
+ *
+ * msg: an entire message from the server
+ *
+ * returns: void
+ */
 void handle_server_message (char *msg) {
         clear_prefix ();
         if (msg[0] == Exit_Message) {
@@ -74,10 +76,15 @@ void handle_server_message (char *msg) {
         }
 }
 
-/* Handles a response from the server telling it to exit. This is the result
- * of the server determining the user has properly issued the \exit command. 
- * You will need to call the c library close function on socket_fd as part
- * of a proper exit. */
+/*
+ * Function: handle_exit
+ * -----------------------
+ *
+ * Handles a response from the server telling it to exit. This is the result
+ * of the server determining the user has properly issued the \exit command.
+ *
+ * returns: void
+ */
 void handle_exit () {
 	printf ("You have left\n");
 	close (socket_fd);
